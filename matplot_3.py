90dd846d3f9d66a0a56faf7f50260c51309f0aaf
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-10-15 04:41:41 -0700

    Add files via upload

---
+++ matplot_3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import random
+random.seed()
+x = []
+y = []
+for i in range(5):
+    x.append(random.randint(0,5))
+for i in range(5):
+    y.append(random.randint(0,5))
+
+
+plt.scatter(x,y)
+plt.show()
